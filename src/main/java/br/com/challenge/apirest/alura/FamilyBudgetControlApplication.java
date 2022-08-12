302a29c6d03630b9e55f01fa160f41b8f96cd845
DyaneAndrade <dyane.aaraujo@gmail.com>
2022-08-12 14:46:30 -0300

    Conexão com bando de dados e inicialização do projeto

---
+++ src/main/java/br/com/challenge/apirest/alura/FamilyBudgetControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.challenge.apirest.alura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyBudgetControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyBudgetControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/challenge/apirest/alura/FamilyBudgetControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.challenge.apirest.alura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyBudgetControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
